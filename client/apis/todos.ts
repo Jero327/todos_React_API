0ebfc4d25841714073f75a62171a9c2a993bd98d
Cong Guo <jeraviah@gmail.com>
2023-12-30 17:24:29 +1300

    Update client API consume URL

--- client/apis/todos.ts
+++ client/apis/todos.ts
@@ -1,22 +1,22 @@
 import request from 'superagent'
 import { newTodo, updatedTodo } from '../../models/todos'
 
-const rootUrl = '/api/v1/todos'
+const rootUrl = 'http://localhost:5258/api/TodoApp/'
 
 export async function getTodos() {
-  return await request.get(rootUrl).then((res) => {
+  return await request.get(`${rootUrl}GetTodos`).then((res) => {
     return res.body
   })
 }
 
 export async function deleteTodoItem(todoId: number) {
-  await request.delete(`${rootUrl}/${todoId}`)
+  await request.delete(`${rootUrl}DeleteTodo?id=${todoId}`)
 }
 
 export async function addTodoItem(newTodo: newTodo) {
-  await request.post(rootUrl).send(newTodo)
+  await request.post(`${rootUrl}AddTodo`).send(newTodo)
 }
 
 export async function editTodoItem(todoId: number, updatedTodo: updatedTodo) {
-  await request.put(`${rootUrl}/${todoId}`).send(updatedTodo)
+  await request.put(`${rootUrl}UpdateTodo?id=${todoId}`).send(updatedTodo)
 }
