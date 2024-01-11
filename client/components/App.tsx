74620619266de883c5fc7c82e79a13907772cf5f
Cong Guo <jeraviah@gmail.com>
2024-01-11 22:09:29 +1300

    Config tailwind css

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="app">
-        <h1>todos</h1>
+        <h1 className="text-3xl font-bold underline">Nothing to do</h1>
         <AddTodo />
         <TodoList />
       </div>
